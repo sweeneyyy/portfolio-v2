c79ba061f318e9aa8db7e40b9c30119ffb204271
sweeneyyy <sweeneyarnett@gmail.com>
2018-07-11 21:42:12 -0700

    nav dropdown home image

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, options);
-});
+$(document).ready(function(){
+  //Nav mobile sidebar
+  $('.sidenav').sidenav();
+  //Nav dropdown menu
+  $(".dropdown-trigger").dropdown({
+    hover: true
+  });
+
+  //Nav dropdown links match
+  $('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if ( location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 800);
+        return false;
+      }
+    }
+  });
+}); // end document ready
